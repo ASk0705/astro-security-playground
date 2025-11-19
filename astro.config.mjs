363c90ad03f609f79d242ec30b2c6d08c85a683b
ASk0705 <skabhay0@gmail.com>
2025-11-19 23:09:18 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-import { squooshImageService } from "astro/assets/services/squoosh"; // ✅ ADD THIS
 
 export default defineConfig({
   output: "server",
-  image: {
-    service: squooshImageService(), // ✅ FIX
-  },
   adapter: cloudflare(),
+  image: {
+    service: "astro/assets/services/noop",
+  }
 });
